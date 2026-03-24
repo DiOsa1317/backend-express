87f1a70079a099a2a0b9515a2bc0776e8dcdaa47
Dilliya <dilusha.37.writer@gmail.com>
2026-03-24 18:04:48 +0400

    do 6?

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 let items = [
-        { 'id': 1, "name": "Максим" },
-        { 'id': 2, "name": "Дилия" }
-    ]
+    { 'id': 1, "name": "Максим" },
+    { 'id': 2, "name": "Дилия" }
+]
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -18,5 +18,17 @@ router.post('/', function(req, res, next) {
     items.push(newUser);
     return res.status(201).json(newUser);
 })
+
+router.get('/', function(req, res, next) {
+    const userId = req.params.id;
+    const user = null;
+    for (const item of items) {
+        if (item['id'] != userId) {
+            continue;
+        }
+        return res.status(201).json(item);
+    }
+    return res.status(404);
+});
 
 module.exports = router;
