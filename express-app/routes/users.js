ec62e8222bbf48ea9ac1c11d306abb07bea1087e
Dilliya <dilusha.37.writer@gmail.com>
2026-03-24 18:43:47 +0400

    dsfdsaf

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -6,28 +6,52 @@ let items = [
     { id: 2, name: "Дилия" }
 ]
 
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db');
+db.run(`CREATE TABLE IF NOT EXISTS users (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name text)`);
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send({
-        "items": items
-    })
+    // res.send({
+    //"items": items
+    //})
+    return db.all("SELECT id, name FROM users", [], (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(rows);
+        }
+    });
 });
 
 router.post('/', function(req, res, next) {
-    const newUser = req.body;
-    items.push(newUser);
-    return res.status(201).json(newUser);
+    const name = req.body;
+    //items.push(newUser);
+    const insert = "INSERT INTO users (name) VALUES (@name)";
+    db.run(insert, [name]);
+    return res.status(201).json(name);
 })
 
 router.get('/:id', function(req, res, next) {
     const userId = parseInt(req.params.id);
-    for (const item of items) {
-        if (item.id !== userId) {
-            continue;
+    //for (const item of items) {
+    //if (item.id !== userId) {
+    // continue;
+    //}
+    //return res.send(res.json(item));
+    //}
+    //return res.send(res.status(404));
+    return db.all("SELECT id, name FROM users WHERE id = @userId", [userId], (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(rows);
         }
-        return res.send(res.json(item));
-    }
-    return res.send(res.status(404));
+    });
 });
+
 
 module.exports = router;
