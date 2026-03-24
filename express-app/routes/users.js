be9bd71870b83ebef4d6f404ccfc66229ff9ecca
Maksim Savinykh <savinykhmaksim062gmail.com>
2026-03-24 18:58:16 +0500

    5

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+let items = [
+        { 'id': 1, "name": "Максим" },
+        { 'id': 2, "name": "Дилия" }
+    ]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send({
-        'items': [
-            { 'id': 1, "name": "Максим" },
-            { 'id': 2, "name": "Дилия" }
-        ]
+        "items": items
     })
 });
+
+router.post('/', function(req, res, next) {
+    const newUser = req.body;
+    items.push(newUser);
+    return res.status(201).json(newUser);
+})
 
 module.exports = router;
